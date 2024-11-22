c0744a5190c189a85d06f1fe98f69c6b84181738
simon-ricotti <60928111+simon-ricotti@users.noreply.github.com>
2024-11-22 10:00:10 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+// Establish a cache name
+const cacheName = 'MyFancyCacheName_v1';
+
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(resources);
+};
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    addResourcesToCache([
+      "/",
+      "/vuelos.html",
+    ]),
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  // Check if this is a navigation request
+  if (event.request.mode === 'navigate') {
+    // Open the cache
+    event.respondWith(caches.open(cacheName).then((cache) => {
+      // Go to the network first
+      return fetch(event.request.url).then((fetchedResponse) => {
+        cache.put(event.request, fetchedResponse.clone());
+
+        return fetchedResponse;
+      }).catch(() => {
+        // If the network is unavailable, get
+        return cache.match(event.request.url);
+      });
+    }));
+  } else {
+    return;
+  }
+});
