37ef9659aae30e8136f1b418a6fb437ca8e28a18
simon-ricotti <60928111+simon-ricotti@users.noreply.github.com>
2024-11-22 12:24:48 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,6 +9,7 @@ const addResourcesToCache = async (resources) => {
 self.addEventListener("install", (event) => {
   event.waitUntil(
     addResourcesToCache([
+      "/",
       "/vuelos/",
       "/vuelos/index.html",
     ]),
